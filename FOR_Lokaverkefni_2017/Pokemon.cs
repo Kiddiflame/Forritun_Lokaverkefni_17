93844caf221706dd365d9f8f9f9bf73c708eff72
Kiddiflame <kiddiflame@gmail.com>
2017-04-25 11:39:53 +0000

    lagfærði og bætti við string, fyrir spilaheiti

---
+++ FOR_Lokaverkefni_2017/Pokemon.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FOR_Lokaverkefni_2017
+{
+    public class Pokemon : Spilastokkar
+    {
+        /*Hrutaspil erfir reglurnar frá spilastokkar en inniheldur sér spilin og statta*/
+
+        public Pokemon(string id, int stat1, int stat2, int stat3, int stat4, int s_defense, int speed)
+        : base(id, stat1, stat2, stat3, stat4)
+        {
+            HP = stat1;
+            Attack = stat2;
+            Defense = stat3;
+            S_Attack = stat4;
+        }
+
+        public int HP { get; set; }
+        public int Attack { get; set; }
+        public int Defense { get; set; }
+        public int S_Attack { get; set; }
+
+    }
+}
+
